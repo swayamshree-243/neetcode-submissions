4ac3e3cc2d4e0fbf55329a44afc8097d3d0c008e
swayamshree-243 <gswayamshree2007@gmail.com>
2026-07-09 21:45:52 +0530

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        if(intervals.empty()) return true;
+        sort(intervals.begin(), intervals.end(), [](const Interval &a, const Interval &b){
+            return a.start < b.start;
+        });
+        priority_queue<int, vector<int>, greater<int>> pq;
+        pq.push(intervals[0].end);
+        for(int i = 1; i < intervals.size(); i++){
+            if(pq.top() <= intervals[i].start) pq.pop();
+            pq.push(intervals[i].end);
+        }
+        if(pq.size() != 1) return false;
+        else return true;
+    }
+};
